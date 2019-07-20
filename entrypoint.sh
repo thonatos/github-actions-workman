23aa598c6609511ac4a3c5394e5e9569081cbfc9
Suyi <thonatos.yang@gmail.com>
2019-07-20 12:42:16 +0800

    feat: rename npm_auth_token to npm_token

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 set -e
 
-# add ssh config
-if [ -n "$RELEASE_SSH_ID_RSA" ] && [ -n "$RELEASE_SSH_ID_RSA" ]; then
-  echo "# add ssh config";
-
-  mkdir -p "$HOME/.ssh";
-  echo "$RELEASE_SSH_ID_RSA" > "$HOME/.ssh/id_rsa";
-  echo "$RELEASE_SSH_ID_RSA_PUB" > "$HOME/.ssh/id_rsa.pub";
-  chmod 400 "$HOME/.ssh/id_rsa";
-  chmod 400 "$HOME/.ssh/id_rsa.pub";
-  git config --global user.name "$RELEASE_GIT_USER_NAME";
-  git config --global user.email "$RELEASE_GIT_USER_EMAIL";
-fi
-
 # add npm config
-if [ -n "$NPM_AUTH_TOKEN" ]; then
+if [ -n "$NPM_TOKEN" ]; then
   echo "# add npm config";
 
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
@@ -29,7 +16,7 @@ if [ -n "$NPM_AUTH_TOKEN" ]; then
   fi
 
   # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$NPM_CONFIG_USERCONFIG";
+  printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$NPM_CONFIG_USERCONFIG";
 
   chmod 0600 "$NPM_CONFIG_USERCONFIG";
 fi
