8a210696348e8dfb0dbf10ebaccce8ca7f04d22d
Suyi <thonatos.yang@gmail.com>
2019-07-29 16:23:34 +0800

    chore: update clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-rm -f index.{js,d.ts,d.ts.map} lib/*.{js,d.ts,d.ts.map} test/*.{js,d.ts,d.ts.map};
+rm -f bin/*.{js,d.ts,d.ts.map} lib/*.{js,d.ts,d.ts.map} lib/**/*.{js,d.ts,d.ts.map} test/*.{js,d.ts,d.ts.map} test/**/*.{js,d.ts,d.ts.map};
