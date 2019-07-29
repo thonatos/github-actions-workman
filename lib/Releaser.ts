e8fdbaca776340ffe594d4d0383da5f59659a942
Suyi <thonatos.yang@gmail.com>
2019-07-29 16:44:41 +0800

    feat: add branch filter

--- lib/Releaser.ts
+++ lib/Releaser.ts
@@ -1,38 +1,51 @@
 import Base from './Base';
 
 export default class Checker extends Base {
-  public async publishNodePackage() {
+
+  public async run(tools: any) {
+    await super.init(tools);
+
+    const { releaseBranch } = tools.arguments;
+    const { event, packageVersion } = this;
+
+    if (event !== 'push') {
+      tools.exit.neutral(`[Releaser] should be triggered by event:push`);
+      return;
+    }
+
+    if (releaseBranch) {
+      const currentBranch = await this.getCurrentBranch();
+      if (currentBranch !== releaseBranch) {
+        tools.exit.neutral(`[Releaser] should be triggered by branch:${releaseBranch}`);
+        return;
+      }
+    }
+
+    tools.log(' Check Commit Message');
+    const commitMessage = await this.getCommitMessage();
+    if (commitMessage !== `Release ${packageVersion}`) {
+      tools.exit.neutral('[Releaser] commit message should include release proposal');
+      return;
+    }
+
+    tools.log(`[Releaser] release npm package ${packageVersion}`);
+    await this.publishNodePackage();
+    tools.exit.success('[Releaser] done!');
+  }
+  private async publishNodePackage() {
     const { pkg, tools } = this;
     tools.log('@@publishNodePackage', pkg);
     // await tools.runInWorkspace('git', ['checkout', 'master']);
     await tools.runInWorkspace('npm', ['publish', '--access', 'public']);
   }
 
-  public async run(tools: any) {
-    await super.init(tools);
-
-    const { event, packageVersion } = this;
-
-    if (event !== 'push') {
-      tools.exit.neutral('Releaser: should be triggered with event:push');
-      return;
-    }
-
-    tools.log('Releaser: Check Commit Message');
-    const commitMessage = await this.getCommitMessage();
-    if (commitMessage !== `Release ${packageVersion}`) {
-      tools.exit.neutral('Releaser: commit message should include release proposal');
-      return;
-    }
-
-    tools.log(`Releaser: release npm package ${packageVersion}`);
-    await this.publishNodePackage();
-    tools.exit.success('Releaser: done!');
+  private async getCurrentBranch() {
+    const { stdout } = await this.tools.runInWorkspace('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
+    return stdout.toString().replace(/\n*$/, '');
   }
 
   private async getCommitMessage() {
-    const { tools } = this;
-    const { stdout } = await tools.runInWorkspace('git', ['log', '--format=%B', '-n', '1']);
+    const { stdout } = await this.tools.runInWorkspace('git', ['log', '--format=%B', '-n', '1']);
     return stdout.toString().replace(/\n*$/, '');
   }
 }
